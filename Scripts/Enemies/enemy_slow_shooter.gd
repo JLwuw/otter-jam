e4a14e83d7796e952bd34cbdae49337f5bc94201
JLwuw <juandaburgoslandazuri@gmail.com>
2026-04-18 12:11:01 -0500

    fix: update enemy class logic

--- Scripts/Enemies/enemy_slow_shooter.gd
+++ Scripts/Enemies/enemy_slow_shooter.gd
@@ -1,9 +1,21 @@
 class_name EnemySlowShooter
 extends Enemy
 
+var 	shoot_component: ComponentBasicShoot 
+
 func _ready() -> void:
 	super._ready()
-	var chase_component: ComponentPlayerChase = $"Player Chase Component"
-	var shoot_component: ComponentBasicShoot = $"Basic Shoot Component"
+	var chase_component: ComponentPlayerChase = get_node_or_null("Player Chase Component")
+	shoot_component = get_node_or_null("Basic Shoot Component")
 	chase_component.player = player
 	shoot_component.player = player
+
+func set_enemy_active(is_active: bool) -> void:
+	super.set_enemy_active(is_active)
+		
+	if shoot_component == null:
+		print("Shoot component not found on", name)
+		return
+	
+	shoot_component.set_process(is_active)
+		
