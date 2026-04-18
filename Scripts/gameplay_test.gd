07a4f360966cd5bb675b5d7e06ddae4abb0187ec
JLwuw <juandaburgoslandazuri@gmail.com>
2026-04-18 11:11:36 -0500

    feat: adds score to debug ui

--- Scripts/gameplay_test.gd
+++ Scripts/gameplay_test.gd
@@ -5,9 +5,13 @@ var enemy_scenes: Array[PackedScene] = [
 	preload("res://Scenes/Enemies/enemy_slow_shooter.tscn")
 ]
 
-@export var fps_update_interval: float = 0.01
+@export var label_update_interval: float = 0.01
+var label_update_timer: float = 0.01
+
 @onready var fps_label: Label = $DebugUI/FPSLabel
-var fps_update_timer: float = 0.01
+@onready var score_label: Label = $DebugUI/ScoreLabel
+@onready var combo_label: Label = $DebugUI/ComboLabel
+@onready var combo_timer_label: Label = $DebugUI/ComboTimerLabel
 
 func _ready() -> void:
 	EnemyDB.init(enemy_scenes)
@@ -15,11 +19,24 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if fps_label == null:
 		return
+	
+	
 
-	fps_update_timer -= delta
-	if fps_update_timer > 0.0:
+	label_update_timer -= delta
+	if label_update_timer > 0.0:
 		return
 
-	fps_update_timer = fps_update_interval
+	label_update_timer = label_update_interval
 	var fps: float = Engine.get_frames_per_second()
 	fps_label.text = "FPS: %.0f" % fps
+	
+	var score: int = ScoreManager.get_final_score()
+	score_label.text = "Score: %.0f" % score
+	
+	var combo: int = ScoreManager.combo
+	combo_label.text = "Combo: %.0f" % combo
+	
+	var combo_timer: float = ScoreManager.combo_timer
+	combo_timer_label.text = "Combo Timer: %.0f" % combo_timer
+	
+	
